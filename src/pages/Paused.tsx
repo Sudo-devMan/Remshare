ef6c86762bf54f405659af30888db79d7392260b
Devman <inevitabledouble.e@gmail.com>
2026-06-06 18:25:01 +0200

    Added document title for maintenance page

--- src/pages/Paused.tsx
+++ src/pages/Paused.tsx
@@ -1,5 +1,6 @@
 
 export default function Paused() {
+	document.title = "Under Maintenance"
 	return (
 		<div className='w-full h-screen text-3xl flex-col bg-green-100 text-blue-800 flex justify-center items-center'>
 			<h1 className='bangers-font text-5xl'>EKSE! WE BUSY BRUH.</h1>
