81e2087c22ff9aa6258c076682067d4ad89e4aac
Devman <inevitabledouble.e@gmail.com>
2026-06-06 18:38:47 +0200

    Added padding for mobile view

--- src/pages/Paused.tsx
+++ src/pages/Paused.tsx
@@ -2,7 +2,7 @@
 export default function Paused() {
 	document.title = "Under Maintenance"
 	return (
-		<div className='w-full h-screen text-3xl flex-col bg-green-100 text-blue-800 flex justify-center items-center'>
+		<div className='w-full h-screen text-3xl p-5 sm:p-1 flex-col bg-green-100 text-blue-800 flex justify-center items-center'>
 			<h1 className='bangers-font text-5xl'>EKSE! WE BUSY BRUH.</h1>
 			<p className='caveat'>Remshare is currently under maintenance. We will be back soon ska wara :)</p>
 		</div>
