4725569b14d477dd762c6e6922a57ead19330a7b
Devman <inevitabledouble.e@gmail.com>
2026-06-09 09:22:12 +0200

    Change background and add back home button below received files div

--- src/pages/ReceiveForm.tsx
+++ src/pages/ReceiveForm.tsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileDownload } from "@fortawesome/free-solid-svg-icons/faFileDownload";
 import api from "../config/api";
 import type { AxiosResponse } from "axios";
+import { Link } from "react-router-dom";
 
 const textInputClasses: string = "rounded-xl p-2 text-2xl border-2 border-blue-800 bg-blue-400 text-blue-950";
 
@@ -106,11 +107,12 @@ function ReceiveForm() {
             {
                 !received ? (
                     <div className="w-full min-h-screen sm:grid sm:grid-cols-2 flex flex-col items-center justify-center p-4 gap-6">
-                        <form method="post" onSubmit={handleSubmit} className="w-full max-w-md">
+                        <form aria-disabled={loading} method="post" onSubmit={handleSubmit} className="w-full max-w-md">
                             <div className="mb-5">
                                 <p className="caveat text-xl">Receiver's email:</p>
                                 <input 
                                     type="email" 
+                                    disabled={loading}
                                     name="receiverEmail" 
                                     value={receiveData.receiverEmail}
                                     onChange={handleChange}
@@ -124,6 +126,7 @@ function ReceiveForm() {
                                 <input 
                                     type="password" 
                                     name="password" 
+                                    disabled={loading}
                                     value={receiveData.password}
                                     onChange={handleChange}
                                     placeholder="files password..." 
@@ -136,6 +139,7 @@ function ReceiveForm() {
                                 <input 
                                     type="text" 
                                     name="uniqueId" 
+                                    disabled={loading}
                                     value={receiveData.uniqueId}
                                     onChange={handleChange}
                                     placeholder="Unique ID..." 
@@ -150,8 +154,12 @@ function ReceiveForm() {
                                         <button type="submit" className="receive-lg w-full sm:w-auto">
                                             <span>RECEIVE <FontAwesomeIcon icon={faFileDownload} /></span>
                                         </button> :
-                                        <button disabled type="submit" className="receive-lg w-full sm:w-auto">
-                                            <span>RECEIVING...</span>
+                                        <button disabled className="receive-lg text-xl w-full sm:w-auto">
+                                            <svg aria-hidden="true" className="w-4 h-4 text-neutral-tertiary animate-spin fill-brand me-2 inline" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                                                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                                            </svg>
+                                            RECEIVING
                                         </button>
                                 }
                             </div>
@@ -204,7 +212,7 @@ function ReceiveForm() {
                                                     return (
                                                         <li key={idx} className="flex items-center justify-between p-2 rounded-lg bg-blue-100 hover:bg-blue-50 transition-colors">
                                                             <span className="break-all pr-4 text-lg font-medium text-blue-950">
-                                                                {cleanName}
+                                                                <a target="_blank" href={fileUrl}>{cleanName}</a>
                                                             </span>
                                                             {fileUrl && (
                                                                 <button 
@@ -254,6 +262,7 @@ function ReceiveForm() {
                                 </section>
                             </div>
                         </div>
+                        <Link className="caveat text-xl underline" to="/">Back home</Link>
                     </div>
                 )
             }
