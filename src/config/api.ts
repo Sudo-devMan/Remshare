f3c18fc71d9d67ebb3a1912d76214e0c68201b1c
Devman <inevitabledouble.e@gmail.com>
2026-06-09 08:21:45 +0200

    Configure axios progress bar

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,22 +1,36 @@
 
 import axios, {type AxiosInstance} from 'axios'
+import nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+nprogress.configure({
+    showSpinner: false,
+    speed: 400,
+    minimum: 0.1
+})
 
 const api: AxiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL
 })
 
-// FOR WHEN I DO AUTH:
-// api.interceptors.request.use(
-//     (config) => {
-//         const token = localStorage.getItem(ACCESS_TOKEN)
-//         if (token) {
-//             config.headers.Authorization = `Bearer ${token}`
-//         }
-//         return config
-//     },
-//     (error) => {
-//         return Promise.reject(error)
-//     }
-// )
+api.interceptors.request.use(
+    (config) => {
+        nprogress.start()
+        return config
+    }, (error) => {
+        nprogress.done()
+        Promise.reject(error)
+    }
+)
+
+api.interceptors.response.use(
+    (response) => {
+        nprogress.done()
+        return response
+    }, (error) => {
+        nprogress.done()
+        return Promise.reject(error)
+    }
+)
 
 export default api
